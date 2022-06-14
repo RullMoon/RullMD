619023c8ebd881664ab4c2309e2bc16d5520ea6a
RullMoon <107094781+RullMoon@users.noreply.github.com>
2022-06-14 09:05:56 +0700

    Update info-bioowner.js

--- plugins/info-bioowner.js
+++ plugins/info-bioowner.js
@@ -1,27 +1,24 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `*───────[ BIODATA OWNER ]───────*
-*💌 Nama* : Sad-Bot
-*✉️ Nama RL* : Fahri
+*💌 Nama* : Moon
+*✉️ Nama RL* : Rull
 *♂️ Gender* : Laki - laki
-*🕋 Agama* : Islam
-*⏰ Tanggal lahir* : 10 Juni 2007
-*🎨 Umur* : 15
-*🧮 Kelas* : 9
-*🧩 Hobby* : Nonton Hanime, Chatting, Recode script bot
-*💬 Sifat* : Idiot, Tidak Ramah, Kanjut, Prik, Pedofil
-*🗺️ Tinggal* : Indo, Riau, Indragiri Hulu, Air Molek
-*❤️ Suka* : warnah kuning & biru, Hanime, waifu, loli, trap, furry, kucing
-*💔 Benci* : autis, anak epep, seleb
+*🕋 Agama* : ;
+*⏰ Tanggal lahir* : ?
+*🎨 Umur* : ?
+*🧮 Kelas* : ??
+*🧩 Hobby* : ?
+*💬 Sifat* : ?
+*🗺️ Tinggal* : ?
+*❤️ Suka* : Dandelion
+*💔 Benci* : ?
 
 *───────[ SOSIAL MEDIA ]───────*
-*📷 instagran* : @bot_whangsaf
-*🇫  Facebook* : Adison Fahri
-*🏮 Chanel Youtube* : Papah-Chan
-*🐈 Github:* FahriAdison
+𝑮 𝑨 𝑲   𝑨 𝑫 𝑨
 
 `
-conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
+conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["𝙼𝙴𝙽𝚄", '.menu'], [null, null],[null,null]], m)
 }
 handler.help = ['bioowner']
 handler.tags = ['info']
